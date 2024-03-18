02d985110b045ff14d826d8ac83bc9b49b46e86d
jra <julien.gault.50@gmail.com>
2024-03-18 18:56:13 +0100

    solution

--- MSLearn.CSharpCreateCSharpMethodsReturnValues.Challenge/Program.cs
+++ MSLearn.CSharpCreateCSharpMethodsReturnValues.Challenge/Program.cs
@@ -12,14 +12,44 @@ void PlayGame()
 
     while (play)
     {
-        var target;
-        var roll;
+        var target = GetTarget();
+        var roll = GetRoll();
 
         Console.WriteLine($"Roll a number greater than {target} to win!");
         Console.WriteLine($"You rolled a {roll}");
-        Console.WriteLine(WinOrLose());
+        Console.WriteLine(WinOrLose(target, roll));
         Console.WriteLine("\nPlay again? (Y/N)");
 
         play = ShouldPlay();
     }
+}
+
+bool ShouldPlay()
+{
+    string? readResult;
+    do
+    {
+        readResult = Console.ReadLine();
+        if (readResult != null)
+        {
+            readResult = readResult.Trim().ToLower();
+        }
+    } while (readResult != "y" && readResult != "n");
+
+    return readResult == "y" ? true : false;
+}
+
+int GetTarget()
+{
+    return random.Next(1, 6);
+}
+
+int GetRoll()
+{
+    return random.Next(1, 7);
+}
+
+string WinOrLose(int target, int roll)
+{
+    return roll > target ? "You win!" : "You lose!";
 }
