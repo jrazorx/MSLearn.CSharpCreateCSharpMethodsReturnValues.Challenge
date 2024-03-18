b98e238e4ae1c3d30232c72ce9e2d1f4e6dd2ef9
jra <julien.gault.50@gmail.com>
2024-03-18 18:34:18 +0100

    Add project files.

---
+++ MSLearn.CSharpCreateCSharpMethodsReturnValues.Challenge/Program.cs
@@ -0,0 +1,25 @@
+﻿Random random = new Random();
+
+Console.WriteLine("Would you like to play? (Y/N)");
+if (ShouldPlay())
+{
+    PlayGame();
+}
+
+void PlayGame()
+{
+    var play = true;
+
+    while (play)
+    {
+        var target;
+        var roll;
+
+        Console.WriteLine($"Roll a number greater than {target} to win!");
+        Console.WriteLine($"You rolled a {roll}");
+        Console.WriteLine(WinOrLose());
+        Console.WriteLine("\nPlay again? (Y/N)");
+
+        play = ShouldPlay();
+    }
+}
